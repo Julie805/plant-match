90f609a533b052398abe4a19f30be21c1b74bc2e
Julie M <seejulielee@gmail.com>
2022-07-26 16:36:29 -0700

    Added navigation

---
+++ project-directory/js/main.js
@@ -0,0 +1,14 @@
+//updates the copyright year in the footer
+const copyrightYear = document.querySelector('#copyright-year');
+copyrightYear.innerText = new Date().getFullYear();
+
+
+//Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon //
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
